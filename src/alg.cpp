bcf589dc59f906ae4275ed2240cae1ff5ab8e652
AleksandrZzz <124875504+AleksandrZzz@users.noreply.github.com>
2023-06-09 18:31:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,42 +1,117 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
 #include <string>
-template<typename T, int size>
-class TStack {
- private:
-    T arr[100];
-    int m;
-
- public:
-    TStack() : m(-1) {}
-    void push(T value) {
-        if (!isFull())
-            arr[++m] = value;
-        else
-            throw std::string("Full");
+#include <map>
+#include "tstack.h"
+int getPrior(char operation) {
+  std::pair<char, int> priority[6];
+  switch (operation) {
+    case'(':
+      priority[0].first = '(';
+      priority[0].second = 0;
+    case')':
+      priority[1].first = ')';
+      priority[1].second = 1;
+    case'+':
+      priority[2].first = '+';
+      priority[2].second = 2;
+    case'-':
+      priority[3].first = '-';
+      priority[3].second = 2;
+    case'*':
+      priority[4].first = '*';
+      priority[4].second = 3;
+    case'/':
+      priority[5].first = '/';
+      priority[5].second = 3;
+  }
+  int prioritet = -1;
+  for (int j = 0; j < 6; ++j) {
+    if (operation == priority[j].first) {
+      prioritet = priority[j].second;
+      break;
     }
-    T get() const {
-        return arr[m];
-    }
-    T pop() {
-        if (isEmpty())
-            throw std::string("Empty");
-        else
-            return arr[m--];
-    }
-    bool isEmpty() const {
-        return m == -1;
-    }
-    bool isFull() const {
-        return m == size - 1;
-    }
-    T top() {
-        if (!isEmpty()) {
-            return arr[m];
-        } else {
-            exit(EXIT_FAILURE);
+  }
+  return prioritet;
+}
+std::string space(const std::string& s) {
+  if (s.length() <= 2) return s;
+  int n = 2 - s.length() % 2;
+  std::string r(s, 0, n);
+  for (auto it = s.begin() + n; it != s.end();) {
+    r += ' '; r += *it++;;
+  }
+  return r;
+}
+int count(const int& a, const int& b, const int& op) {
+  switch (op) {
+    default:
+      break;
+    case'+': return a + b;
+    case'-': return a - b;
+    case'*': return a * b;
+    case'/': return a / b;
+  }
+  return 0;
+}
+std::string infx2pstfx(std::string inf) {
+  std::string work;
+  Tstack<char, 100> stack1;
+  for (auto& op : inf) {
+    int prior = getPrior(op);
+    if (prior == -1) {
+      work += op;
+    } else {
+      if (stack1.get() < prior || prior == 0 || stack1.isEmpty()) {
+        stack1.push(op);
+      } else if (op == ')') {
+        char sm = stack1.get();
+        while (getPrior(sm) >= prior) {
+          work += sm;
+          stack1.pop();
+          sm = stack1.get();
         }
+        stack1.pop();
+      } else {
+        char sm = stack1.get();
+        while (getPrior(sm) >= prior) {
+          work += sm;
+          stack1.pop();
+          sm = stack1.get();
+        }
+        stack1.push(op);
+      }
     }
-};
-#endif
+  }
+  while (!stack1.isEmpty()) {
+    work += stack1.get();
+    stack1.pop();
+  }
+  work = space(work);
+  return work;
+}
+
+int eval(std::string pref) {
+  Tstack<int, 100> stack1;
+  std::string num = "";
+  for (size_t i = 0; i < pref.size(); i++) {
+    if (getPrior(pref[i]) == -1) {
+      if (pref[i] == ' ') {
+        continue;
+      } else if (isdigit(pref[i + 1])) {
+        num += pref[i];
+        continue;
+      } else {
+        num += pref[i];
+        stack1.push(atoi(num.c_str()));
+        num = "";
+      }
+    } else {
+      int b = stack1.get();
+      stack1.pop();
+      int a = stack1.get();
+      stack1.pop();
+      stack1.push(count(a, b, pref[i]));
+    }
+  }
+  return stack1.get();
+}
